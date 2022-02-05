b8f95baaf2d83e9ca17670b97bc7095c7fea120c
yeggh <ygn.gharedaghi@gmail.com>
2022-02-05 21:14:31 +0330

    client streaming

--- client.py
+++ client.py
@@ -1,7 +1,17 @@
 from libtcp import build_client
 import os
 
+def establish_UDP_connection(port, ip):
+    print("established")
+    pass
+
 def on_message(message):
+    message = message.split()
+    if message[0] == '[PORTREQ]':
+        port = int(message[1])
+        ip = message[2]
+        establish_UDP_connection(port, ip)
+    
     # In ja momkene bekhaym if bezanim age folan chiz bood udp va cv2 va ina ro
     # roshan konim
     print(message, flush=True, end="")
--- streamingserver.py
+++ streamingserver.py
@@ -1,8 +1,35 @@
 from libtcp import build_server
+import socket
+
+client_watching = []
 
 main_menu = "Welcome to choghondar\nFilmi nadarim felan\n"
+path = ""
 
 def handler(message, id, callback):
+    if id in client_watching:
+        if message == "/exit":
+            client_watching.remove(id)
+    else:
+        try:
+            message = int(message)
+        except ValueError:
+            callback("not a number!")
+        client_watching.append(id)
+        if message == 1:
+            path = "TeenageMutantNinjaTurtles.mp4"
+
+        server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+#sock = socket.socket()
+        server_socket.bind(('', 0))
+        port = server_socket.getsockname()[1]
+
+        host_name  = socket.gethostname()
+        host_ip = socket.gethostbyname(host_name)
+    
+    
+        callback('[{}] {} {}'.format('PORTREQ', port, host_ip))
+
     callback("$start_udp_and_play_video")
 
 build_server(handler, 10003, main_menu)
