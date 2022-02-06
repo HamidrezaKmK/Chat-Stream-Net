d1876ae9ece8fdf3e3d3c9675391633225c529ee
hkalbasi <hamidrezakalbasi@protonmail.com>
2022-02-06 11:44:34 +0330

    move udp connection to main thread

--- client.py
+++ client.py
@@ -1,6 +1,10 @@
 from libtcp import build_client
 import os
 import clientstream
+from queue import Queue
+import socket, threading, random
+
+q = Queue()
 
 def establish_UDP_connection(port, ip):
     print("established.")
@@ -13,7 +17,7 @@ def on_message(message):
     if message_splitted[0] == '[PORTREQ]':
         port = int(message_splitted[1])
         ip = message_splitted[2]
-        establish_UDP_connection(port, ip)
+        q.put((port, ip))
     
     # In ja momkene bekhaym if bezanim age folan chiz bood udp va cv2 va ina ro
     # roshan konim
@@ -24,6 +28,14 @@ def on_exit():
 
 send_message = build_client(on_message, on_exit, 10001)
 
+def inp():
+    while True:
+        message = input()
+        send_message(message)
+
+tinp = threading.Thread(target=inp)
+tinp.start()
+
 while True:
-    message = input()
-    send_message(message)
+    port, ip = q.get()
+    establish_UDP_connection(port, ip)
